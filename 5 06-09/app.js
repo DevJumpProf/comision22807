b2d13af030e501767a0a0fa4ff70a0b441f5d46c
Arbusta <gabriel.munoz@arbusta.net>
2022-09-06 23:01:30 -0300

    JS1

---
+++ 5 06-09/app.js
@@ -0,0 +1,77 @@
+
+/* Case Sensitive */
+
+/*   Clave - valor DATO */
+
+let nombre = "Bruno Diaz" // string - cadena de texto
+let Nombre = "Batman"
+
+
+/* Tipado débil */
+
+nombre = "Oswald"
+nombre = 748 // number - numero
+
+nombre = true // Boolean - boleano 
+
+/* UpperCamelCase */
+let NombreApellido = "Bruno Diaz"
+
+/* lowerCamelCase */
+
+let nombreApellido = "Bruno Diaz"
+
+/* snake_case */
+
+let nombre_apellido = "Bruno Diaz"
+
+let $nombre = "Marc Anthony"
+
+
+
+var nombre2 = 'Juan'; 
+let apellido = 'Perez';// Vamos a usar principalemente esta forma
+const comidaFavorita = 'Pizza'; //
+
+nombre2= "Pepe"
+
+
+/* ## Tipos de Datos */
+
+let texto = "Hola Mundo";  // string
+
+let texto2 = "3" ; // string
+
+let texto3 = "como estas?";
+
+let numero1 = 1985 ; // number
+
+let numero2 = 45; // number
+
+let booleano = false ;// false
+
+/* console.log ("hola")
+console.log(2+2)
+document.write (numero2*booleano ) */
+
+/*  document.write (texto + " " + texto3 )  */// concatenar
+/* console.log(comidaFavorita) */
+
+/* alert () */
+
+/* let nombreLet= prompt ("Hola Dime tu nombre" )
+
+alert ("hola " + nombreLet) 
+ */
+
+let num2 =parseInt(prompt ("Hola un numero" ))
+let num3 =parseInt(prompt ("Hola otro numero" ))
+// Number
+let suma = num2+num3
+
+alert (suma) 
+
+
+
+
+
