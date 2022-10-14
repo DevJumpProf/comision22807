fa017bf9aa9328061227438efe0a66ec669cf776
Arbusta <gabriel.munoz@arbusta.net>
2022-10-14 00:09:28 -0300

    Arrays - Objetos

---
+++ Codigo/14 13-10/objetos.js
@@ -0,0 +1,20 @@
+ const heroe = {
+    //clave    valor
+    nombre : "Batman",
+    ciudad: "Gotica",
+    ligaJusticia: true,
+    alterEgo: "Bruno diaz",
+    enemigos: 32,
+    amigos: ["Superman","Robin","Batichica", "Wonder Woman"]
+     
+ };
+
+ console.log(`${heroe.nombre} vive en ${heroe.ciudad}`)
+
+
+
+
+
+
+
+
